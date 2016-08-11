4aaf26cb9d40aeec78630a7a74f312fc28b6adfe
jbpc <johnblackphone@icloud.com>
2016-08-12 04:39:54 +0800

    Hi John

---
+++ 06_Status_Network.h
@@ -0,0 +1,6 @@
+char Status_Network(long *Network_Sent, long *Network_Received, double *Network_Rate){
+//*************************************************************************************************************************
+//
+	
+	return 'V';
+}
