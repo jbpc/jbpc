417bd1a83bfd9a34f5fd18408609027089ba5bb6
jbpc <johnblackphone@icloud.com>
2016-08-12 04:56:30 +0800

    Add files via upload

---
+++ 06_Status_Network.h
@@ -0,0 +1,8 @@
+char Status_Network(long *Network_Sent, long *Network_Received, double *Network_Rate){
+//*************************************************************************************************************************
+//
+	
+    printf("Is it?");
+    printf
+	return 'V';
+}
