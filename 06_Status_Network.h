0b2c362adca2811b50d9643d582b1449ad8522b2
jbpc <johnblackphone@icloud.com>
2016-08-12 05:06:46 +0800

    John

--- 06_Status_Network.h
+++
@@ -1,8 +0,0 @@
-char Status_Network(long *Network_Sent, long *Network_Received, double *Network_Rate){
-//*************************************************************************************************************************
-//
-	
-    printf("Is it?");
-    printf
-	return 'V';
-}
